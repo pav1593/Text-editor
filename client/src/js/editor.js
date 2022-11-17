989e68806c1b2e9907913553c3b164fdbf845dda
Nick Pavlovic <nickpavlovic@icloud.com>
2022-11-16 22:28:16 -0500

    changes to database save and editor display

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -24,9 +24,14 @@ export default class {
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+
+    
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      if(!data) {
+        this.editor.setValue(localData || header);
+      }
+      this.editor.setValue(`${data.value}`);
     });
 
     this.editor.on('change', () => {
